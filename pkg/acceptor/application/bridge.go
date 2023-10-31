52573ea9a746f72fba8c535bba8313dc1ebb0b33
Alexandre Thibault <alexandre.thibault@artex.io>
2023-10-31 14:52:00 +0100

    Simplify message forwarding
    Signed-off-by: Alexandre Thibault <alexandre.thibault@artex.io>

--- pkg/acceptor/application/bridge.go
+++ pkg/acceptor/application/bridge.go
@@ -4,19 +4,11 @@ import (
 	"github.com/rs/zerolog"
 
 	"github.com/quickfixgo/enum"
-	"github.com/quickfixgo/field"
 	"github.com/quickfixgo/quickfix"
 	"github.com/quickfixgo/tag"
 
 	"sylr.dev/fix/pkg/utils"
 )
-
-type FixTypes struct {
-	fixString  quickfix.FIXString
-	fixDecimal quickfix.FIXDecimal
-	fixInt     quickfix.FIXInt
-	fixTime    quickfix.FIXUTCTimestamp
-}
 
 func NewBridge() *Bridge {
 	bridge := Bridge{
@@ -42,6 +34,8 @@ func NewBridge() *Bridge {
 	bridge.router.AddRoute(quickfix.BeginStringFIX44, string(enum.MsgType_ORDER_MASS_CANCEL_REPORT), bridge.onMassCancelReportExchange)
 	bridge.router.AddRoute(quickfix.BeginStringFIX44, string(enum.MsgType_QUOTE_STATUS_REPORT), bridge.onQuoteStatusReportExchange)
 
+	bridge.router.AddRoute(quickfix.BeginStringFIX42, string(enum.MsgType_BUSINESS_MESSAGE_REJECT), bridge.onBusinessMessageReject)
+	bridge.router.AddRoute(quickfix.BeginStringFIX44, string(enum.MsgType_BUSINESS_MESSAGE_REJECT), bridge.onBusinessMessageReject)
 	bridge.router.AddRoute(quickfix.ApplVerIDFIX50SP2, string(enum.MsgType_BUSINESS_MESSAGE_REJECT), bridge.onBusinessMessageReject)
 
 	return &bridge
@@ -112,132 +106,31 @@ func (app *Bridge) FromApp(message *quickfix.Message, sessionID quickfix.Session
 
 /////////////// Client messages
 
-func (app *Bridge) onNewOrderSingleClient(order *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
-	if len(app.connectedExchanges) == 0 {
-		return quickfix.NewMessageRejectError("No connected exchanges", int(tag.BusinessRejectReason), nil)
-	}
-	target := app.connectedExchanges[0]
-
-	clOrdId, err := order.Body.GetString(tag.ClOrdID)
-	if err != nil {
-		return quickfix.NewMessageRejectError("Missing ClOrdID", int(tag.BusinessRejectReason), nil)
-	}
-
-	app.orderMapping[clOrdId] = sessionID
-
-	message := quickfix.NewMessage()
-	message.Header.Set(field.NewBeginString(target.BeginString))
-
-	message.Header.SetField(tag.MsgType, field.NewMsgType(enum.MsgType_ORDER_SINGLE))
-	message.Body.SetString(tag.ClOrdID, clOrdId)
-
-	var fixType FixTypes
-	if err := utils.QuickFixMessageCopyFields(&message.Body, order.Body, []utils.FieldDescription{
-		{&fixType.fixDecimal, tag.OrderQty, true},
-		{&fixType.fixInt, tag.OrdType, true},
-		{&fixType.fixDecimal, tag.Price, false},
-		{&fixType.fixInt, tag.Side, true},
-		{&fixType.fixString, tag.Symbol, true},
-		{&fixType.fixInt, tag.TimeInForce, true},
-		{&fixType.fixTime, tag.TransactTime, true},
-		{&fixType.fixInt, tag.OrderOrigination, false},
-	}); err != nil {
-		return err
-	}
-	if err := utils.QuickFixMessageCopyPartyIds(&message.Body, order.Body); err != nil {
-		return err
-	}
-
-	if err := quickfix.SendToTarget(message, target); err != nil {
-		return quickfix.NewMessageRejectError(err.Error(), int(tag.BusinessRejectReason), nil)
-	}
-	return nil
+func (app *Bridge) onNewOrderSingleClient(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
+	return app.forwardClientMessageToExchange(msg, sessionID)
 }
 
 func (app *Bridge) onOrderCancelRequestClient(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
-	if len(app.connectedExchanges) == 0 {
-		return quickfix.NewMessageRejectError("No connected exchanges", int(tag.BusinessRejectReason), nil)
-	}
-	target := app.connectedExchanges[0]
-
-	clOrdId, err := msg.Body.GetString(tag.ClOrdID)
-	if err != nil {
-		return quickfix.NewMessageRejectError("Missing ClOrdID", int(tag.BusinessRejectReason), nil)
-	}
-
-	app.orderMapping[clOrdId] = sessionID
-
-	message := quickfix.NewMessage()
-	message.Header.Set(field.NewBeginString(target.BeginString))
-
-	message.Header.SetField(tag.MsgType, field.NewMsgType(enum.MsgType_ORDER_CANCEL_REQUEST))
-	message.Body.SetString(tag.ClOrdID, clOrdId)
-
-	var fixType FixTypes
-	if err := utils.QuickFixMessageCopyFields(&message.Body, msg.Body, []utils.FieldDescription{
-		{&fixType.fixString, tag.OrigClOrdID, false},
-		{&fixType.fixString, tag.OrderID, false},
-		{&fixType.fixInt, tag.Side, true},
-		{&fixType.fixString, tag.Symbol, false},
-		{&fixType.fixTime, tag.TransactTime, true},
-	}); err != nil {
-		return err
-	}
-	if err := utils.QuickFixMessageCopyPartyIds(&message.Body, msg.Body); err != nil {
-		return err
-	}
-
-	if err := quickfix.SendToTarget(message, target); err != nil {
-		return quickfix.NewMessageRejectError(err.Error(), int(tag.BusinessRejectReason), nil)
-	}
-	return nil
+	return app.forwardClientMessageToExchange(msg, sessionID)
 }
 
 func (app *Bridge) onOrderCancelReplaceRequestClient(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
-	if len(app.connectedExchanges) == 0 {
-		return quickfix.NewMessageRejectError("No connected exchanges", int(tag.BusinessRejectReason), nil)
-	}
-	target := app.connectedExchanges[0]
-
-	clOrdId, err := msg.Body.GetString(tag.ClOrdID)
-	if err != nil {
-		return quickfix.NewMessageRejectError("Missing ClOrdID", int(tag.BusinessRejectReason), nil)
-	}
-
-	app.orderMapping[clOrdId] = sessionID
-
-	message := quickfix.NewMessage()
-	message.Header.Set(field.NewBeginString(target.BeginString))
-
-	message.Header.SetField(tag.MsgType, field.NewMsgType(enum.MsgType_ORDER_CANCEL_REPLACE_REQUEST))
-	message.Body.SetString(tag.ClOrdID, clOrdId)
-
-	var fixType FixTypes
-	if err := utils.QuickFixMessageCopyFields(&message.Body, msg.Body, []utils.FieldDescription{
-		{&fixType.fixString, tag.OrigClOrdID, false},
-		{&fixType.fixString, tag.OrderID, false},
-		{&fixType.fixDecimal, tag.OrderQty, true},
-		{&fixType.fixInt, tag.OrdType, true},
-		{&fixType.fixDecimal, tag.Price, false},
-		{&fixType.fixInt, tag.Side, true},
-		{&fixType.fixString, tag.Symbol, false},
-		{&fixType.fixInt, tag.TimeInForce, true},
-		{&fixType.fixTime, tag.TransactTime, true},
-		{&fixType.fixInt, tag.OrderOrigination, false},
-	}); err != nil {
-		return err
-	}
-	if err := utils.QuickFixMessageCopyPartyIds(&message.Body, msg.Body); err != nil {
-		return err
-	}
-
-	if err := quickfix.SendToTarget(message, target); err != nil {
-		return quickfix.NewMessageRejectError(err.Error(), int(tag.BusinessRejectReason), nil)
-	}
-	return nil
+	return app.forwardClientMessageToExchange(msg, sessionID)
 }
 
 func (app *Bridge) onMassCancelRequestClient(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
+	return app.forwardClientMessageToExchange(msg, sessionID)
+}
+
+func (app *Bridge) onQuoteClient(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
+	return app.forwardClientMessageToExchange(msg, sessionID)
+}
+
+func (app *Bridge) onQuoteCancelClient(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
+	return app.forwardClientMessageToExchange(msg, sessionID)
+}
+
+func (app *Bridge) forwardClientMessageToExchange(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
 	if len(app.connectedExchanges) == 0 {
 		return quickfix.NewMessageRejectError("No connected exchanges", int(tag.BusinessRejectReason), nil)
 	}
@@ -250,182 +143,59 @@ func (app *Bridge) onMassCancelRequestClient(msg *quickfix.Message, sessionID qu
 
 	app.orderMapping[clOrdId] = sessionID
 
-	message := quickfix.NewMessage()
-	message.Header.Set(field.NewBeginString(target.BeginString))
-
-	message.Header.SetField(tag.MsgType, field.NewMsgType(enum.MsgType_ORDER_MASS_CANCEL_REQUEST))
-	message.Body.SetString(tag.ClOrdID, clOrdId)
-
-	var fixType FixTypes
-	if err := utils.QuickFixMessageCopyFields(&message.Body, msg.Body, []utils.FieldDescription{
-		{&fixType.fixInt, tag.Side, true},
-		{&fixType.fixString, tag.Symbol, true},
-		{&fixType.fixTime, tag.TransactTime, true},
-		{&fixType.fixInt, tag.MassCancelRequestType, true},
-	}); err != nil {
-		return err
-	}
-	if err := utils.QuickFixMessageCopyPartyIds(&message.Body, msg.Body); err != nil {
-		return err
-	}
-
-	if err := quickfix.SendToTarget(message, target); err != nil {
+	if err := quickfix.SendToTarget(msg, target); err != nil {
 		return quickfix.NewMessageRejectError(err.Error(), int(tag.BusinessRejectReason), nil)
 	}
 	return nil
-}
-
-func (app *Bridge) onQuoteClient(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
-	return app.unhandledMessage(msg, sessionID)
-}
-
-func (app *Bridge) onQuoteCancelClient(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
-	return app.unhandledMessage(msg, sessionID)
 }
 
 /////////////// Exchange messages
 
 func (app *Bridge) onExecutionReportExchange(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
-	clOrdId, err := msg.Body.GetString(tag.ClOrdID)
-	if err != nil {
-		return quickfix.NewMessageRejectError("Missing ClOrdID", int(tag.BusinessRejectReason), nil)
-	}
-
-	clientSessionID, found := app.orderMapping[clOrdId]
-	if !found {
-		app.Logger.Warn().Str("clOrdId", clOrdId).Str("session", sessionID.String()).Msg("No client session found for ClOrdID")
-		return nil
-	}
-
-	message := quickfix.NewMessage()
-	message.Header.Set(field.NewBeginString(clientSessionID.BeginString))
-
-	message.Header.SetField(tag.MsgType, field.NewMsgType(enum.MsgType_EXECUTION_REPORT))
-	message.Body.SetString(tag.ClOrdID, clOrdId)
-
-	var fixType FixTypes
-	if err := utils.QuickFixMessageCopyFields(&message.Body, msg.Body, []utils.FieldDescription{
-		{&fixType.fixInt, tag.Account, false},
-		{&fixType.fixString, tag.OrderID, true},
-		{&fixType.fixString, tag.SecondaryOrderID, false},
-		{&fixType.fixString, tag.OrigClOrdID, false},
-		{&fixType.fixString, tag.ExecID, true},
-		{&fixType.fixString, tag.TrdMatchID, false},
-		{&fixType.fixInt, tag.ExecType, true},
-		{&fixType.fixInt, tag.OrdStatus, true},
-		{&fixType.fixString, tag.Symbol, true},
-		{&fixType.fixInt, tag.OrdType, true},
-		{&fixType.fixInt, tag.TimeInForce, true},
-		{&fixType.fixInt, tag.Side, true},
-		{&fixType.fixDecimal, tag.Price, false},
-		{&fixType.fixDecimal, tag.LastPx, false},
-		{&fixType.fixDecimal, tag.CumQty, true},
-		{&fixType.fixDecimal, tag.OrderQty, false},
-		{&fixType.fixDecimal, tag.LeavesQty, true},
-		{&fixType.fixDecimal, tag.LastQty, false},
-		{&fixType.fixString, tag.Text, false},
-		{&fixType.fixTime, tag.TransactTime, true},
-	}); err != nil {
-		return err
-	}
-	if err := utils.QuickFixMessageCopyPartyIds(&message.Body, msg.Body); err != nil {
-		return err
-	}
-
-	if err := quickfix.SendToTarget(message, clientSessionID); err != nil {
-		return quickfix.NewMessageRejectError(err.Error(), int(tag.BusinessRejectReason), nil)
-	}
-	return nil
+	return app.forwardExchangeMessageToClient(msg, sessionID)
 }
 
 func (app *Bridge) onOrderCancelRejectExchange(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
-	clOrdId, err := msg.Body.GetString(tag.ClOrdID)
-	if err != nil {
-		return quickfix.NewMessageRejectError("Missing ClOrdID", int(tag.BusinessRejectReason), nil)
-	}
-
-	clientSessionID, found := app.orderMapping[clOrdId]
-	if !found {
-		app.Logger.Warn().Str("clOrdId", clOrdId).Str("session", sessionID.String()).Msg("No client session found for ClOrdID")
-		return nil
-	}
-
-	message := quickfix.NewMessage()
-	message.Header.Set(field.NewBeginString(clientSessionID.BeginString))
-
-	message.Header.SetField(tag.MsgType, field.NewMsgType(enum.MsgType_ORDER_CANCEL_REJECT))
-	message.Body.SetString(tag.ClOrdID, clOrdId)
-
-	var fixType FixTypes
-	if err := utils.QuickFixMessageCopyFields(&message.Body, msg.Body, []utils.FieldDescription{
-		{&fixType.fixInt, tag.Account, false},
-		{&fixType.fixString, tag.OrderID, true},
-		{&fixType.fixString, tag.SecondaryOrderID, false},
-		{&fixType.fixInt, tag.OrdStatus, true},
-		{&fixType.fixString, tag.OrigClOrdID, false},
-		{&fixType.fixString, tag.Text, false},
-		{&fixType.fixTime, tag.TransactTime, true},
-		{&fixType.fixInt, tag.CxlRejReason, false},
-		{&fixType.fixString, tag.CxlRejResponseTo, true},
-	}); err != nil {
-		return err
-	}
-	if err := utils.QuickFixMessageCopyPartyIds(&message.Body, msg.Body); err != nil {
-		return err
-	}
-
-	if err := quickfix.SendToTarget(message, clientSessionID); err != nil {
-		return quickfix.NewMessageRejectError(err.Error(), int(tag.BusinessRejectReason), nil)
-	}
-	return nil
+	return app.forwardExchangeMessageToClient(msg, sessionID)
 }
 
 func (app *Bridge) onMassCancelReportExchange(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
-	clOrdId, err := msg.Body.GetString(tag.ClOrdID)
-	if err != nil {
-		return quickfix.NewMessageRejectError("Missing ClOrdID", int(tag.BusinessRejectReason), nil)
-	}
-
-	clientSessionID, found := app.orderMapping[clOrdId]
-	if !found {
-		app.Logger.Warn().Str("clOrdId", clOrdId).Str("session", sessionID.String()).Msg("No client session found for ClOrdID")
-		return nil
-	}
-
-	message := quickfix.NewMessage()
-	message.Header.Set(field.NewBeginString(clientSessionID.BeginString))
-
-	message.Header.SetField(tag.MsgType, field.NewMsgType(enum.MsgType_ORDER_MASS_CANCEL_REPORT))
-	message.Body.SetString(tag.ClOrdID, clOrdId)
-
-	var fixType FixTypes
-	if err := utils.QuickFixMessageCopyFields(&message.Body, msg.Body, []utils.FieldDescription{
-		{&fixType.fixString, tag.MassActionReportID, false},
-		{&fixType.fixInt, tag.MassCancelRequestType, true},
-		{&fixType.fixInt, tag.MassCancelResponse, true},
-		{&fixType.fixInt, tag.MassCancelRejectReason, true},
-		{&fixType.fixString, tag.Text, false},
-		{&fixType.fixTime, tag.TransactTime, false},
-	}); err != nil {
-		return err
-	}
-	if err := utils.QuickFixMessageCopyPartyIds(&message.Body, msg.Body); err != nil {
-		return err
-	}
-
-	if err := quickfix.SendToTarget(message, clientSessionID); err != nil {
-		return quickfix.NewMessageRejectError(err.Error(), int(tag.BusinessRejectReason), nil)
-	}
-	return nil
+	return app.forwardExchangeMessageToClient(msg, sessionID)
 }
 
 func (app *Bridge) onQuoteStatusReportExchange(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
 	return app.unhandledMessage(msg, sessionID)
 }
 
+func (app *Bridge) forwardExchangeMessageToClient(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
+	clOrdId, err := msg.Body.GetString(tag.ClOrdID)
+	if err != nil {
+		return quickfix.NewMessageRejectError("Missing ClOrdID", int(tag.BusinessRejectReason), nil)
+	}
+
+	clientSessionID, found := app.orderMapping[clOrdId]
+	if !found {
+		app.Logger.Warn().Str("clOrdId", clOrdId).Str("session", sessionID.String()).Msg("No client session found for ClOrdID")
+		return nil
+	}
+
+	if err := quickfix.SendToTarget(msg, clientSessionID); err != nil {
+		return quickfix.NewMessageRejectError(err.Error(), int(tag.BusinessRejectReason), nil)
+	}
+	return nil
+}
+
 /////////////// BusinessMessageReject messages
 
 func (app *Bridge) onBusinessMessageReject(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
+	if sessionID.IsFIXT() {
+		if len(app.connectedExchanges) == 0 {
+			return quickfix.NewMessageRejectError("No connected exchanges", int(tag.BusinessRejectReason), nil)
+		}
+		if err := quickfix.SendToTarget(msg, app.connectedExchanges[0]); err != nil {
+			return quickfix.NewMessageRejectError(err.Error(), int(tag.BusinessRejectReason), nil)
+		}
+	}
 	return app.unhandledMessage(msg, sessionID)
 }
 
